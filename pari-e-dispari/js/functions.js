bfbedfe1aa4cbd3fdce8183f4fcb9de0f25996f5
andrea-bertaglia <andreabertaglia@gmail.com>
2024-04-04 22:16:11 +0200

    fix: exercise 'pari e dispari' DOM elements

--- pari-e-dispari/js/functions.js
+++ pari-e-dispari/js/functions.js
@@ -1,6 +1,10 @@
 // test
 console.log('Hello Functions');
 
+/**
+ * Description Richiede all'utente di scegliere tra 'pari' o 'dispari' e verifica che non inserisca altre parole
+ * @returns {string} scelta dell'utente tra 'pari' o 'dispari'
+ */
 function userChoice() {
     let userPreference = prompt('Scegli pari o dispari').toLocaleLowerCase();
     
@@ -12,6 +16,10 @@ function userChoice() {
 }
 
 
+/**
+ * Description Richiede all'utente un numero tra 1 e 5
+ * @returns {number} numero scelto dall'utente nell'intervallo da 1 a 5
+ */
 function userNumber() {
     let userNum = parseInt(prompt('Scegli un numero tra 1 e 5'));
 
@@ -23,11 +31,20 @@ function userNumber() {
     return userNum;
 }
 
+/**
+ * Description Genera un numero random per il computer
+ * @returns {number} il numero random generato per il computer
+ */
 function pcNumber() {
     const pcRandomNum = Math.floor(Math.random() * 5) + 1;
     return pcRandomNum;
 }
 
+/**
+ * Description Verifica se il risultato della somma dei due numeri è un numero pari
+ * @param {calcSum} calcSum somma del numero utente e del numero random
+ * @returns {boolean} true se il risultato della somma è pari
+ */
 function isEven(calcSum) {
     let result = true;
 
--- pari-e-dispari/js/script.js
+++ pari-e-dispari/js/script.js
@@ -25,8 +25,32 @@ if (isEven(sum)) {
 }
 
 // Output
+let resultText = "";
+
 if (inputChoice === checkWin) {
-    console.log("Hai vinto! - Ha vinto l'utente");
+    resultText = "Hai vinto!";
+    console.log("Vince l'utente");
 } else {
-    console.log("Hai perso! - Ha vinto il pc");
+    resultText = "Hai perso!";
+    console.log("Vince il pc");
 }
+
+const elemBody = document.querySelector('body');
+const elemResult1 = document.createElement('p');
+const elemResult2 = document.createElement('p');
+const elemResult3 = document.createElement('p');
+const elemResult4 = document.createElement('p');
+const elemResult5 = document.createElement('p');
+const elemResult6 = document.createElement('h2');
+elemResult1.innerHTML = `Pari o Dispari: ${inputChoice}`;
+elemResult2.innerHTML = `Il tuo numero: ${inputNumber}`
+elemResult3.innerHTML = `Il numero del pc: ${randomNumber}`
+elemResult4.innerHTML = `Somma dei due numeri: ${sum}`
+elemResult5.innerHTML = `Il risultato è ${checkWin}`
+elemResult6.innerHTML = `${resultText}`
+elemBody.append(elemResult1);
+elemBody.append(elemResult2);
+elemBody.append(elemResult3);
+elemBody.append(elemResult4);
+elemBody.append(elemResult5);
+elemBody.append(elemResult6);
